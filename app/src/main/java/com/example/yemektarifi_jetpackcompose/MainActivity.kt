8eb576ef9306108458cda1d835852a802cbe48fd
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-17 00:53:29 +0300

    Topbar ve status bar

--- app/src/main/java/com/example/yemektarifi_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/yemektarifi_jetpackcompose/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.yemektarifi_jetpackcompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.yemektarifi_jetpackcompose.ui.theme.YemekTarifiJetpackComposeTheme
 
@@ -28,9 +34,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Sayfa() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Yemek Tarifi") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.anaRenk),
+                    titleContentColor = colorResource(id = R.color.white),
+                )
+            )
+        },
+        content = {
 
+        }
+    )
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/yemektarifi_jetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/yemektarifi_jetpackcompose/ui/theme/Theme.kt
@@ -2,6 +2,8 @@ package com.example.yemektarifi_jetpackcompose.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import android.view.Window
+import android.view.WindowManager
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -9,8 +11,19 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import com.example.yemektarifi_jetpackcompose.R
 
+
+private fun setStatusBarColor(window: Window, color: Color) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = color.toArgb()
+    }
+}
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
@@ -43,12 +56,17 @@ fun YemekTarifiJetpackComposeTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
+
+
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+    val context = LocalContext.current
+    val window: Window = (context as Activity).window
+    setStatusBarColor(window, colorResource(id = R.color.anaRenkKoyu))
 
     MaterialTheme(
         colorScheme = colorScheme,
